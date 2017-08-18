b589f91075691c4bac88f5d904312fd16ba647e2
sen <374362311@qq.com>
2017-08-18 20:26:35 +0800

    mall

---
+++ src/main/java/com/atguigu/service/impl/LoginServiceImpl.java
@@ -0,0 +1,10 @@
+package com.atguigu.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.atguigu.service.LoginService;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+}
